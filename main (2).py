025171e5dd9454f5a86bf964fa1d8d2600986ba4
JordanBakar <113556116+JordanBakar@users.noreply.github.com>
2022-11-16 15:15:03 -0600

    GUI Files

---
+++ main (2).py
@@ -0,0 +1,104 @@
+import sys
+import platform
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+
+#This imports the UI design from the Loading Screen
+from ui_loading_screen import Ui_LoadingScreen
+
+#This imports the UI design from the Main Screen
+from ui_main import Ui_MainWindow
+
+# Counter for the Percentage
+counter = 0
+
+# Main Screen
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # This removes the title section of the screen and makes a floating window
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+
+
+
+
+# Loading Screen
+class LoadingScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_LoadingScreen()
+        self.ui.setupUi(self)
+
+    # UI Code
+
+
+        # This removes the title section of the screen and makes a floating window
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+
+        # Creates drop shadow effect
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 60))
+        self.ui.dropShadowFrame.setGraphicsEffect(self.shadow)
+
+        # This starts the QTimer for the progress bar
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        # TIMER IN MILLISECONDS
+        self.timer.start(50)
+
+        # Changes the description of the subtitle after a certain millisecond count
+
+        # Our programs slogan
+        self.ui.label_description.setText("THE WORLD'S BEST TYPING ASSISTANT")
+
+        # Changes the description of the subtitle after a certain millisecond count
+        QtCore.QTimer.singleShot(3000, lambda: self.ui.label_description.setText("<strong>LOADING</strong> DATABASE"))
+        QtCore.QTimer.singleShot(5000, lambda: self.ui.label_description.setText("<strong>LOADING</strong> USER INTERFACE"))
+
+
+        # This causes the main screen to load after the loading screen closes
+        self.show()
+
+
+    # App functions
+    def progress(self):
+
+        global counter
+
+        # Sets the value of the counter to the initial value
+        self.ui.progressBar.setValue(counter)
+
+        # Creates a clone of the loading screen and opens up the main screen
+        if counter > 100:
+            # Stops the timer
+            self.timer.stop()
+
+            # Shows the main screen
+            self.main = MainWindow()
+            self.main.show()
+
+            # Closes the loading screen
+            self.close()
+
+        # increments the counter by 1 until it hits 100.
+        counter += 1
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LoadingScreen()
+    sys.exit(app.exec_())
---
+++ ui_loading_screen.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'loading_screenwjJZEg.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_LoadingScreen(object):
+    def setupUi(self, LoadingScreen):
+        if not LoadingScreen.objectName():
+            LoadingScreen.setObjectName(u"LoadingScreen")
+        LoadingScreen.resize(680, 400)
+        self.centralwidget = QWidget(LoadingScreen)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(10, 10, 10, 10)
+        self.dropShadowFrame = QFrame(self.centralwidget)
+        self.dropShadowFrame.setObjectName(u"dropShadowFrame")
+        self.dropShadowFrame.setStyleSheet(u"QFrame{\n"
+"	\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0.443, x2:1, y2:0.466, stop:0 rgba(0, 68, 99, 255), stop:1 rgba(0, 29, 42, 255));\n"
+"	color: rgb(230, 230, 230);\n"
+"	border-radius: 12px;\n"
+"}")
+        self.dropShadowFrame.setFrameShape(QFrame.StyledPanel)
+        self.dropShadowFrame.setFrameShadow(QFrame.Raised)
+        self.label_title = QLabel(self.dropShadowFrame)
+        self.label_title.setObjectName(u"label_title")
+        self.label_title.setGeometry(QRect(0, 50, 661, 101))
+        font = QFont()
+        font.setFamily(u"Segoe UI")
+        font.setPointSize(40)
+        self.label_title.setFont(font)
+        self.label_title.setStyleSheet(u"color: rgb(66, 255, 161);")
+        self.label_title.setAlignment(Qt.AlignCenter)
+        self.label_description = QLabel(self.dropShadowFrame)
+        self.label_description.setObjectName(u"label_description")
+        self.label_description.setGeometry(QRect(0, 140, 661, 51))
+        font1 = QFont()
+        font1.setFamily(u"Segoe UI")
+        font1.setPointSize(15)
+        self.label_description.setFont(font1)
+        self.label_description.setStyleSheet(u"color: rgb(17, 204, 221)")
+        self.label_description.setAlignment(Qt.AlignCenter)
+        self.progressBar = QProgressBar(self.dropShadowFrame)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(50, 250, 561, 21))
+        self.progressBar.setStyleSheet(u"QProgressBar {\n"
+"	\n"
+"	background-color: rgb(98, 114, 164);\n"
+"	color: rgb(200, 200, 200);\n"
+"	border-style: none;\n"
+"	border-radius: 10px;\n"
+"	text-align: center;\n"
+"}\n"
+"QProgressBar::chunk{\n"
+"	border-radius: 10px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0.472, x2:1, y2:0.466, stop:0 rgba(255, 212, 101, 255), stop:1 rgba(255, 133, 11, 255));\n"
+"}")
+        self.progressBar.setValue(24)
+        self.label_loading = QLabel(self.dropShadowFrame)
+        self.label_loading.setObjectName(u"label_loading")
+        self.label_loading.setGeometry(QRect(0, 270, 661, 51))
+        font2 = QFont()
+        font2.setFamily(u"Segoe UI")
+        font2.setPointSize(12)
+        self.label_loading.setFont(font2)
+        self.label_loading.setStyleSheet(u"color: rgb(17, 204, 221)")
+        self.label_loading.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.dropShadowFrame)
+
+        LoadingScreen.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(LoadingScreen)
+
+        QMetaObject.connectSlotsByName(LoadingScreen)
+    # setupUi
+
+    def retranslateUi(self, LoadingScreen):
+        LoadingScreen.setWindowTitle(QCoreApplication.translate("LoadingScreen", u"MainWindow", None))
+        self.label_title.setText(QCoreApplication.translate("LoadingScreen", u"<html><head/><body><p><span style=\" font-weight:600;\">PRECISION</span> | SEARCH</p></body></html>", None))
+        self.label_description.setText(QCoreApplication.translate("LoadingScreen", u"THE WORLD'S BEST TYPING ASSISTANT", None))
+        self.label_loading.setText(QCoreApplication.translate("LoadingScreen", u"loading.....", None))
+    # retranslateUi
+
